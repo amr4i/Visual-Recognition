74ef63919ab5ce5a0c25ba8d1fa8cfe861a28c3f
Aarsh Agarwal <19aarsh@gmail.com>
2019-02-04 15:02:24 +0530

    Imgages are read

--- read.py
+++ read.py
@@ -1,4 +1,16 @@
 import os, sys
 import numpy as np 
 
-path = ''
+input_path = str(sys.argv[1])
+
+image_class_folders = [os.path.join(input_path, f) for f in os.listdir(input_path)]
+images_list = [0]*len(image_class_folders)
+counter = 0;
+for image_class in image_class_folders:
+	images_list[counter] = [os.path.join(image_class,f) for f in os.listdir(image_class)]
+	counter += 1;
+
+print images_list
+
+
+
