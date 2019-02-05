2ef3594f378ce34cb003e77ab2191ad4d5b806ae
Aarsh Agarwal <19aarsh@gmail.com>
2019-02-05 17:14:58 +0530

    Updated for label correction

--- read.py
+++ read.py
@@ -5,19 +5,25 @@ import cv2
 def load_images(input_path):
 
 	image_class_folders = [os.path.join(input_path, f) for f in os.listdir(input_path)]
-	image_folders = [str(f) for f in os.listdir(input_path)]
+	# image_folders = [str(f) for f in os.listdir(input_path)]
 	images_list = [0]*len(image_class_folders)
 	counter = 0;
 	image_labels = [0]*len(image_class_folders)
 
 	for image_class in image_class_folders:
 		images_list[counter] = [os.path.join(image_class,f) for f in os.listdir(image_class)]
-		image_labels[counter] = [os.path.join(image_folders[counter], f) for f in os.listdir(image_class)]
+		# image_labels[counter] = [os.path.join(image_folders[counter], f) for f in os.listdir(image_class)]
 		counter += 1;
+
+	counter = 0
+	for image_class in images_list:
+		image_labels[counter] = [image_name.replace(input_path+"/","") for image_name in image_class]
+		counter += 1
 
 	Images = [0]*len(image_class_folders);
 
 	counter = 0;
+	print image_labels
 
 	for image_class in images_list:
 		Images[counter] = [cv2.imread(image, 0) for image in image_class]
